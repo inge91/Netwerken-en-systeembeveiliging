d2ddfb06db51c9f7cb84dcd33d0b6e1dbc9f0215
Inge <inge.becht91@gmail.com>
2012-11-30 13:08:53 +0100

    Created empty handle message function

--- Assignment8/lab8-skeleton.py
+++ Assignment8/lab8-skeleton.py
@@ -6,14 +6,17 @@
 import sys
 import struct
 import random
+import select
 from gui import MainWindow
 from sensor import *
 from socket import *
 import time
 
-def send_ping():
-    message_encode(MSG_PING, 0, 
+# Sends a ping message to MCAST_GRP
 
+def send_ping(socket):
+    encripted_message = message_encode(MSG_PING )
+    socket.sendto(encripted_message, (MCAST_GRP, MCAST_PORT))
     
 
 def socket_subscribe_mcast(sock, ip):
@@ -22,6 +25,14 @@ def socket_subscribe_mcast(sock, ip):
     """
     mreq = struct.pack("4sl", inet_aton(ip), INADDR_ANY)
     sock.setsockopt(IPPROTO_IP, IP_ADD_MEMBERSHIP, mreq)
+
+# Handles depending on the type of message received
+def handle_message(message, address):
+    #First decript the message
+    decripted_message = message_decode(message)
+    if decripted_message[0] == MSG_PING:
+        print "Receiev a ping message"
+ 
 
 def main(argv):
     """
@@ -36,13 +47,18 @@ def main(argv):
     # ALSO: In the lab room change the port to MCAST_PORT + group number
     # so that messages rom different groups do not interfere.
     # When you hand in your code in it must listen on (MCAST_GRP, MCAST_PORT).
+
+
+    # Make resuable
+    mcast.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
     mcast.bind((MCAST_GRP, MCAST_PORT))
+
 
     ## Create the peer-to-peer socket.
     peer = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
     # Set the socket multicast TTL so it can send multicast messages.
     peer.setsockopt(IPPROTO_IP, IP_MULTICAST_TTL, 2)
-    print IP_MULTICAST_TTL
     # Bind the socket to a random port.
     peer.bind(('', INADDR_ANY))
 
@@ -62,17 +78,34 @@ def main(argv):
     window.writeln("sensor value = " + str(value))
 
     # Send ping to all other users and start clock
-    send_ping()
+    send_ping(peer)
 
     start = time.time()
+    
+    # Input from select
+    input = [mcast, peer]
 
     while window.update():
 
         # In case 5 seconds have passed resend ping message
         if (time.time() - start > PING_PERIOD):
-            send_ping()
+            send_ping(peer)
             start = time.time()
         pass
+
+        descriptors, _, _ = select.select(input, [], [])
+        
+        for descriptor in descriptors:
+            # Receive the message from every descriptor
+            message, address = descriptor.recvfrom(1024)
+            print message
+            print "address"
+            print address
+            handle_message(message, address)
+            
+            
+
+    
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
